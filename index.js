3bafbdf1f95b46f5317737ec4963b877a37f60f9
Leonardo <leonardosyo@gmail.com>
2021-01-16 13:53:38 -0300

    now mutation can handle with errors 3

--- index.js
+++ index.js
@@ -68,9 +68,9 @@ export async function astridMutation({ mutation={}, variables={} }) {
     },
     body: JSON.stringify({query, variables})
     })
-    //const json = await response.json();
+    const json = await response.json();
     
-    return response
+    return json
 }
 
 // TODO IMPROVE ERROR HANDLINGg

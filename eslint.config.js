e0cfd023b3f725a61dce6fd0095b61e60e5a7814
nexxus <leonardoso@pm.me>
2026-06-20 17:37:22 -0300

    s

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "coverage", "eslint.config.js"] },
+  { ignores: ["dist", "coverage", "eslint.config.js", "scripts/*.mjs"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
